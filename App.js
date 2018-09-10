21e747b77b1da86d59c740f8773550c5c74c1952
Edwin Mak <cintosyntax@gmail.com>
2018-09-09 21:02:33 -0400

    Add start & stop method used control timer

--- App.js
+++ App.js
@@ -1,13 +1,43 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import TimeDisplay from './TimeDisplay'
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      seconds: 0,
+      tickingProcess: undefined
+    };
+
+    this.start = this.start.bind(this);
+    this.stop = this.stop.bind(this);
+  }
+
+  start() {
+    this.setState(() => {
+      return {
+        tickingProcess: setInterval(() => {
+          this.setState(() => {
+            return { seconds: this.state.seconds + 1 }
+          })
+        }, 1000)
+      }
+    })
+  }
+
+  stop() {
+    this.setState(() => {
+      clearInterval(this.state.tickingProcess);
+      return { tickingProcess: undefined }
+    })
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <TimeDisplay seconds={this.state.seconds}/>
       </View>
     );
   }
--- App.test.js
+++ App.test.js
@@ -1,9 +1,61 @@
 import React from 'react';
 import App from './App';
+import TimeDisplay from './TimeDisplay';
 
 import renderer from 'react-test-renderer';
 
-it('renders without crashing', () => {
-  const rendered = renderer.create(<App />).toJSON();
-  expect(rendered).toBeTruthy();
-});
+jest.useFakeTimers();
+
+describe('App', () => {
+  let mountedApp;
+  const app = () => {
+    if (!mountedApp) {
+      mountedApp = renderer.create(<App/>)
+    }
+    return mountedApp;
+  };
+
+  beforeEach(() => {
+    mountedApp = undefined;
+  })
+
+  describe('rendered TimeDisplay', () => {
+    let timeDisplay = app().root.findByType(TimeDisplay)
+
+    it('should exist', () => {
+      expect(timeDisplay).toBeDefined();
+    })
+
+    it('should be given seconds', () => {
+      expect(timeDisplay.instance.props.seconds).toBe(app().root.instance.state.seconds)
+    })
+  })
+
+  describe('start', () => {
+    beforeEach(() => {
+      app().root.instance.start();
+    })
+
+    it('should increment seconds count every second', () => {
+      let initSeconds = app().root.instance.state.seconds;
+      jest.advanceTimersByTime(1000);
+      expect(app().root.instance.state.seconds).toBe(initSeconds+1);
+      jest.advanceTimersByTime(1000);
+      expect(app().root.instance.state.seconds).toBe(initSeconds+2);
+    })
+  })
+
+  describe('stop', () => {
+    beforeEach(() => {
+      app().root.instance.start();
+      app().root.instance.stop();
+    })
+
+    it('should not increment seconds count every second', () => {
+      let initSeconds = app().root.instance.state.seconds;
+      jest.advanceTimersByTime(1000);
+      expect(app().root.instance.state.seconds).toBe(initSeconds);
+    })
+  })
+
+})
